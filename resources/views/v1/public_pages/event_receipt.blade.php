2c07191296af50c05ece2b61ea209287f7fa50c5
Phil Cartagena <register@fierce.net>
2017-03-26 23:55:54 -0400

    receipt display #5

--- resources/views/v1/public_pages/event_receipt.blade.php
+++ resources/views/v1/public_pages/event_receipt.blade.php
@@ -11,28 +11,23 @@
     @include('v1.parts.start_content', ['header' => "Registration Confirmation", 'subheader' => '', 'o' => '2', 'w1' => '8', 'w2' => '8', 'r1' => 0, 'r2' => 0, 'r3' => 0])
     <div class="whole">
 
-        <div style="float: right;" class="col-md-4 col-sm-4">
-            <img style="opacity: .25;" src="/images/meeting.jpg" width="100%" height="90%">
-        </div>
-        <div class="left col-md-8 col-sm-8">
-            <div class="myrow col-md-12 col-sm-12">
-                <div class="col-md-2 col-sm-2" style="text-align:center;">
-                    <h1 class="fa fa-5x fa-calendar"></h1>
+        <div class="myrow col-md-12 col-sm-12">
+            <div class="col-md-2 col-sm-2" style="text-align:center;">
+                <h1 class="fa fa-5x fa-calendar"></h1>
+            </div>
+            <div class="col-md-7 col-sm-7">
+                <h2><b>{{ $event->eventName }}</b></h2>
+                <div style="margin-left: 10px;">
+                    {{ $event->eventStartDate->format('n/j/Y g:i A') }}
+                    - {{ $event->eventEndDate->format('n/j/Y g:i A') }}
+                    <br>
+                    {{ $loc->locName }}<br>
+                    {{ $loc->addr1 }} <i class="fa fa-circle fa-tiny-circle"></i> {{ $loc->city }}
+                    , {{ $loc->state }} {{ $loc->zip }}
                 </div>
-                <div class="col-md-7 col-sm-7">
-                    <h2><b>{{ $event->eventName }}</b></h2>
-                    <div style="margin-left: 10px;">
-                        {{ $event->eventStartDate->format('n/j/Y g:i A') }}
-                        - {{ $event->eventEndDate->format('n/j/Y g:i A') }}
-                        <br>
-                        {{ $loc->locName }}<br>
-                        {{ $loc->addr1 }} <i class="fa fa-circle fa-tiny-circle"></i> {{ $loc->city }}
-                        , {{ $loc->state }} {{ $loc->zip }}
-                    </div>
-                    <br/>
-                </div>
-                <div class="col-md-3 col-sm-3">
-                </div>
+                <br/>
+            </div>
+            <div class="col-md-3 col-sm-3">
             </div>
         </div>
     </div>
