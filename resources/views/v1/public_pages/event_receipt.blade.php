10c6c0b0a09e415ed2b51dce94deb2dabe0c1d94
Phil Cartagena <register@fierce.net>
2017-04-04 01:13:00 -0400

    minor receipt fix

--- resources/views/v1/public_pages/event_receipt.blade.php
+++ resources/views/v1/public_pages/event_receipt.blade.php
@@ -55,139 +55,8 @@ foreach($array as $chap) {
                 </div>
             </div>
 
-            @if($rf->seats > 1)
-                @for($i=$rf->regID-1;$i<=$rf->regID;$i++)
-                    <?php
-                    $reg = \App\Registration::find($i); $tcount++;
-                    $person = \App\Person::find($reg->personID);
-                    ?>
-
-                    <div class="myrow col-md-12 col-sm-12">
-                        <div class="col-md-2 col-sm-2" style="text-align:center;">
-                            <h1 class="fa fa-5x fa-user"></h1>
-                        </div>
-                        <div class="col-md-10 col-sm-10">
-                            <table class="table table-bordered table-condensed table-striped">
-                                <tr>
-                                    <th colspan="4" style="text-align: left;">{{ strtoupper($reg->membership) }} TICKET:
-                                        #{{ $tcount }}</th>
-                                </tr>
-                                <tr>
-                                    <th style="text-align: left; color:darkgreen;">Ticket</th>
-                                    <th style="text-align: left; color:darkgreen;">Original Cost</th>
-                                    <th style="text-align: left; color:darkgreen;">Discounts</th>
-                                    <th style="text-align: left; color:darkgreen;">Subtotal</th>
-                                </tr>
-                                <tr>
-                                    <td style="text-align: left;">{{ $ticket->ticketLabel }}</td>
-
-                                    <td style="text-align: left;"><i class="fa fa-dollar"></i>
-                                        @if($reg->membership == 'Member')
-                                            {{ number_format($ticket->memberBasePrice, 2, ".", ",") }}
-                                        @else
-                                            {{ number_format($ticket->nonmbrBasePrice, 2, ".", ",") }}
-                                        @endif
-                                    </td>
-
-                                    @if(!($ticket->earlyBirdEndDate === null) && $ticket->earlyBirdEndDate->diffInSeconds($today)>0)
-                                        @if($rf->discountCode)
-                                            <td style="text-align: left;">Early Bird, {{ $rf->discountCode }}</td>
-                                        @else
-                                            <td style="text-align: left;">Early Bird</td>
-                                        @endif
-                                    @else
-                                        @if($rf->discountCode)
-                                            <td style="text-align: left;">{{ $rf->discountCode }}</td>
-                                        @else
-                                            <td style="text-align: left;"> --</td>
-                                        @endif
-                                    @endif
-                                    <td style="text-align: left;"><i class="fa fa-dollar"></i>
-                                        {{ number_format($reg->subtotal, 2, ".", ",") }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th colspan="2" style="width: 50%; text-align: left;">Attendee Info</th>
-                                    <th colspan="2" style="width: 50%; text-align: left;">Event-Specific Info</th>
-                                </tr>
-                                <tr>
-                                    <td colspan="2" style="text-align: left;">
-                                        @if($person->prefix)
-                                            {{ $person->prefix }}
-                                        @endif
-                                        {{ $person->firstName }}
-                                        @if($person->prefName)
-                                            ({{ $person->prefName }})
-                                        @endif
-
-                                        @if($person->midName !== null)
-                                            {{ $person->midName }}
-                                        @endif
-                                        {{ $person->lastName }}
-
-                                        @if($person->suffix)
-                                            {{ $person->suffix }}
-                                        @endif
-                                        [ {{ $person->login }} ]
-                                        <br/>
-                                        @if($person->compName)
-                                            @if($person->title)
-                                                {{ $person->title }}
-                                            @else
-                                                Employed
-                                            @endif
-                                            at {{ $person->compName }}
-                                        @else
-                                            {{ $person->title }}
-                                        @endif
-                                        @if($person->indName)
-                                            in the {{ $person->indName }} industry <br/>
-                                        @endif
-
-                                        @if($person->affiliation)
-                                            <br/>Affiliated with: {{ $person->affiliation }}
-                                        @endif
-                                    </td>
-                                    <td colspan="2" style="text-align: left;">
-                                        @if($reg->isFirstEvent)
-                                            <b>First Event?</b> {{ $reg->isFirstEvent }}<br/>
-                                        @endif
-
-                                        <b>Add to Roster:</b> {{ $reg->canNetwork }}<br/>
-                                        <b><a data-toggle="tooltip" title="Do you authorize PMI to submit your PDUs?">PDU
-                                                Submission :</a></b> {{ $reg->isAuthPDU }}<br/>
-                                        @if($reg->eventQuestion)
-                                            <p><b>Speaker Questions:</b> {{ $reg->eventQuestion }}</p>
-                                        @endif
-
-                                        @if($reg->eventTopics)
-                                            <p><b>Future Topics:</b><br/> {{ $reg->eventTopics }}</p>
-                                        @endif
-
-                                        @if($reg->cityState)
-                                            <br/><b>Commuting From:</b> {{ $reg->cityState }}</br>
-                                        @endif
-
-                                        @if($reg->specialNeeds)
-                                            <b>Special Needs:</b> {{ $reg->specialNeeds }}<br/>
-                                        @endif
-
-                                        @if($reg->allergenInfo)
-                                            <b>Dietary Info:</b> {{ $reg->allergenInfo }}<br/>
-                                            {{ $reg->eventNotes }}
-                                        @elseif($reg->eventNotes)
-                                            <b>Other Comments/Notes:</b> {{ $reg->eventNotes }}
-                                        @endif
-
-                                    </td>
-                                </tr>
-                            </table>
-                        </div>
-
-                    </div>
-                @endfor
-            @else
-                <?php
+            @for($i=$rf->regID-($rf->seats-1);$i<=$rf->regID;$i++)
+               <?php
                 $reg = \App\Registration::find($rf->regID); $tcount++;
                 $person = \App\Person::find($reg->personID);
                 $ticket = \App\Ticket::find($reg->ticketID);
@@ -248,7 +117,7 @@ foreach($array as $chap) {
                                     @endif
                                     {{ $person->firstName }}
                                     @if($person->prefName)
-                                        {{ $person->prefName }}
+                                        ({{ $person->prefName }})
                                     @endif
                                     @if($person->midName)
                                         {{ $person->midName }}
@@ -257,7 +126,7 @@ foreach($array as $chap) {
                                     @if($person->suffix)
                                         {{ $person->suffix }}
                                     @endif
-                                    [ {{ $person->login }}" ]
+                                    <nobr>[ {{ $person->login }}" ]</nobr>
                                     <br/>
                                     @if($person->compName)
                                         @if($person->title)
@@ -320,7 +189,7 @@ foreach($array as $chap) {
 
                 </div>
 
-            @endif
+            @endfor
 
             <div class="myrow col-md-12 col-sm-12">
                 <div class="col-md-2 col-sm-2" style="text-align:center;">
