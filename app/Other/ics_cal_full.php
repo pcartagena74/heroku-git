2384e8d2872973e896b896891d90a740df829753
pcartagena74 <register@fierce.net>
2018-11-16 14:59:36 -0500

    ICal HotFix - ics_cal_full: takes event collection

--- app/Other/ics_cal_full.php
+++ app/Other/ics_cal_full.php
@@ -10,6 +10,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use App\Org;
 use App\Location;
+use Illuminate\Database\Eloquent\Collection as Collection;
 
 class ics_cal_full
 {
@@ -29,8 +30,12 @@ class ics_cal_full
     private $org;
     private $o_string;
 
-    public function __construct(\App\Event $event)
-    {
+    public function __construct(Collection $events) {
+
+        $this->events = $events;
+        $this->org = Org::find($events->first()->orgID);
+
+        /*
         $etype             = DB::table('org-event_types')
                                ->where('etID', $event->eventTypeID)
                                ->select('etName')
@@ -54,6 +59,7 @@ class ics_cal_full
         $this->uid         = $event->eventStartDate->format('Ymd\THis') . $event->eventID . "@mcentric.org";
         $this->stamp       = Carbon::now()->format('Ymd\THis');
         $this->contact     = $event->contactEmail;
+        */
     }
 
     private function _escapeString($string)
@@ -67,34 +73,66 @@ class ics_cal_full
         return $this->o_string;
     }
 
+    public function open(){
+        return "BEGIN:VCALENDAR\r\n".
+               "VERSION:2.0\r\n".
+               "PRODID:-//mCentric-hosted " . $this->org->orgName . " Event" ."\r\n".
+               "METHOD:REQUEST\r\n";
+    }
+
+    public function close(){
+        return "END:VCALENDAR\r\n";
+    }
+
     private function _generate()
     {
 
-        $this->o_string = "BEGIN:VCALENDAR\r\n".
-            "VERSION:2.0\r\n".
-            "PRODID:-//mCentric-hosted " . $this->org . " Event" ."\r\n".
-            "METHOD:REQUEST\r\n".
-            "BEGIN:VEVENT\r\n".
-            "SUBJECT:".$this->_escapeString($this->description)."\r\n".
-            "SUMMARY:".$this->_escapeString($this->description)."\r\n".
-            "UID:". $this->uid ."\r\n".
-            "SEQUENCE:0\r\n".
-            "CLASS:PUBLIC"."\r\n".
-            "CREATED:".$this->created->format('Ymd\THis')."\r\n".
-            "DTSTART;TZID=" . $this->tzid . ":".$this->start->format('Ymd\THis')."\r\n".
-            "DTEND;TZID=" . $this->tzid . ":".$this->end->format('Ymd\THis')."\r\n".
-            "CATEGORIES:".$this->_escapeString($this->categories)."\r\n".
-            "LOCATION:".$this->_escapeString($this->location)."\r\n".
-            "URL;VALUE=URI:".$this->_escapeString($this->uri)."\r\n".
-            "TRANSP:OPAQUE"."\r\n".
-            "DTSTAMP:". $this->stamp ."\r\n".
-            "LAST-MODIFIED:" . $this->updated->format('Ymd\THis') . "\r\n".
-            "ORGANIZER;CN=".$this->_escapeString($this->org).":MAILTO:" . $this->contact . "\r\n".
-            "X-MICROSOFT-CDO-BUSYSTATUS:Confirmed"."\r\n".
-            "X-MICROSOFT-CDO-INTENDEDSTATUS:Confirmed"."\r\n".
-            "DESCRIPTION:".$this->_escapeString($this->html)."\r\n".
-            "END:VEVENT\r\n".
-            "END:VCALENDAR\r\n";
+        $this->o_string = '';
+        foreach($this->events as $event){
+            $loc               = Location::find($event->locationID);
+            $etype             = DB::table('org-event_types')
+                                    ->where('etID', $event->eventTypeID)
+                                    ->select('etName')
+                                    ->first();
+
+            $this->start       = $event->eventStartDate;
+            $this->end         = $event->eventEndDate;
+            $this->created     = $event->createDate;
+            $this->updated     = $event->updateDate;
+            $this->html        = wordwrap($event->eventDescription, 75, "\r\n", TRUE);
+            $this->summary     = trans('messages.email_txt.for_det_visit') . ": " . env('APP_URL') . "/events/" . $event->slug;
+            $this->categories  = $etype->etName;
+            $this->description = $event->eventLabel;
+            $this->tzid        = DB::table('timezone')->where('zoneOffset', '=', $event->eventTimeZone)->select('tzid')->first();
+            $this->tzid        = str_replace(" ", "_", $this->tzid->tzid);
+            $this->location    = $loc->locName . " " . $loc->addr1 . ", " . $loc->addr2 . ", " . $loc->city . ", " . $loc->state . " " . $loc->zip;
+            $this->uri         = env('APP_URL') . "/events/" . $event->slug;
+            $this->uid         = $event->eventStartDate->format('Ymd\THis') . $event->eventID . "@mcentric.org";
+            $this->stamp       = Carbon::now()->format('Ymd\THis');
+            $this->contact     = $event->contactEmail;
+
+            $this->o_string .=
+                "BEGIN:VEVENT\r\n".
+                "SUBJECT:".$this->_escapeString($this->description)."\r\n".
+                "SUMMARY:".$this->_escapeString($this->description)."\r\n".
+                "UID:". $this->uid ."\r\n".
+                "SEQUENCE:0\r\n".
+                "CLASS:PUBLIC"."\r\n".
+                "CREATED:".$this->created->format('Ymd\THis')."\r\n".
+                "DTSTART;TZID=" . $this->tzid . ":".$this->start->format('Ymd\THis')."\r\n".
+                "DTEND;TZID=" . $this->tzid . ":".$this->end->format('Ymd\THis')."\r\n".
+                "CATEGORIES:".$this->_escapeString($this->categories)."\r\n".
+                "LOCATION:".$this->_escapeString($this->location)."\r\n".
+                "URL;VALUE=URI:".$this->_escapeString($this->uri)."\r\n".
+                "TRANSP:OPAQUE"."\r\n".
+                "DTSTAMP:". $this->stamp ."\r\n".
+                "LAST-MODIFIED:" . $this->updated->format('Ymd\THis') . "\r\n".
+                "ORGANIZER;CN=".$this->_escapeString($this->org->orgName).":MAILTO:" . $this->contact . "\r\n".
+                "X-MICROSOFT-CDO-BUSYSTATUS:Confirmed"."\r\n".
+                "X-MICROSOFT-CDO-INTENDEDSTATUS:Confirmed"."\r\n".
+                "DESCRIPTION:".$this->_escapeString($this->html)."\r\n".
+                "END:VEVENT\r\n";
+        }
     }
     /*
 
