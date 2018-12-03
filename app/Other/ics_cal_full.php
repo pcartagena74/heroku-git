c488430a2b240240e9c8254475136b93767ed78c
pcartagena74 <register@fierce.net>
2018-12-03 15:22:06 -0500

    cleanup

--- app/Other/ics_cal_full.php
+++ app/Other/ics_cal_full.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Comment:
+ * Comment: PMI MassBay-specific output of complete event listing
  * Created: 4/16/2017
  */
 
@@ -34,32 +34,6 @@ class ics_cal_full
 
         $this->events = $events;
         $this->org = Org::find($events->first()->orgID);
-
-        /*
-        $etype             = DB::table('org-event_types')
-                               ->where('etID', $event->eventTypeID)
-                               ->select('etName')
-                               ->first();
-        $org               = Org::find($event->orgID);
-        $loc               = Location::find($event->locationID);
-
-        $this->start       = $event->eventStartDate;
-        $this->end         = $event->eventEndDate;
-        $this->created     = $event->createDate;
-        $this->updated     = $event->updateDate;
-        $this->html        = $event->eventDescription;
-        $this->org         = $org->orgName;
-        $this->summary     = trans('messages.email_txt.for_det_visit') . ": " . env('APP_URL') . "/events/" . $event->slug;
-        $this->categories  = $etype->etName;
-        $this->description = $event->eventLabel;
-        $this->tzid        = DB::table('timezone')->where('zoneOffset', '=', $event->eventTimeZone)->select('tzid')->first();
-        $this->tzid        = str_replace(" ", "_", $this->tzid->tzid);
-        $this->location    = $loc->locName . " " . $loc->addr1 . ", " . $loc->addr2 . ", " . $loc->city . ", " . $loc->state . " " . $loc->zip;
-        $this->uri         = env('APP_URL') . "/events/" . $event->slug;
-        $this->uid         = $event->eventStartDate->format('Ymd\THis') . $event->eventID . "@mcentric.org";
-        $this->stamp       = Carbon::now()->format('Ymd\THis');
-        $this->contact     = $event->contactEmail;
-        */
     }
 
     private function _escapeString($string)
@@ -136,35 +110,4 @@ class ics_cal_full
                 "END:VEVENT\r\n";
         }
     }
-    /*
-
-            "X-ALT-DESC;FMTTYPE=text/html:".$this->_escapeString($this->html)."\r\n" . $this->uri . "\n".
-
-BEGIN:VCALENDAR
-PRODID:-//mCentric-Hosted Events
-VERSION:2.0
-METHOD:REQUEST
-X-MS-OLK-FORCEINSPECTOROPEN:TRUE
-BEGIN:VEVENT
-ATTENDEE;CN=Name;RSVP=TRUE:mailto:{{ $event->contactEmail }}
-CLASS:PUBLIC
-CREATED:{{ $event->createDate->format('Ymd\THis') }}
-DESCRIPTION:{{ $org->orgName }} {{ $event->etName }}
-DTSTART;TZID={{ $tzid }}:{{ $event->eventStartDate->format('Ymd\THis') }}
-DTEND;TZID={{ $tzid }}:{{ $event->eventEndDate->format('Ymd\THis') }}
-DTSTAMP:{{ \Carbon\Carbon::now()->format('Ymd\THis') }}
-LAST-MODIFIED:{{ $event->updateDate->format('Ymd\THis') }}
-ORGANIZER;CN=name:mailto:{{ $event->contactEmail }}
-PRIORITY:5
-SEQUENCE:0
-LOCATION:{{ $loc->locName }} {{ $loc->addr1 }} {{ $loc->addr2 }} {{ $loc->city }}, {{ $loc->state }} {{ $loc->zip }}
-SUMMARY:{{ $event_url }}
-TRANSP:OPAQUE
-UID:{{ $event->eventStartDate->format('Ymd\THis') }}@mcentric.org
-X-MICROSOFT-CDO-BUSYSTATUS:Confirmed
-X-MICROSOFT-CDO-INTENDEDSTATUS:Confirmed
-END:VEVENT
-END:VCALENDAR
-
-     */
 }
