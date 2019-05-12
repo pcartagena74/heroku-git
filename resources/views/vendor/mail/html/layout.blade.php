bd7209bb49d1d2e830ee88c783bb45cdefa0f53f
pcartagena74 <register@fierce.net>
2019-05-12 14:29:32 -0400

    template change

--- resources/views/vendor/mail/html/layout.blade.php
+++ resources/views/vendor/mail/html/layout.blade.php
@@ -27,7 +27,7 @@
         <tr>
             <td align="center">
                 <table class="content" width="100%" cellpadding="0" cellspacing="0">
-                    {{ $header or '' }}
+                    {{ $header ?? '' }}
 
                     <!-- Email Body -->
                     <tr>
@@ -38,7 +38,7 @@
                                     <td class="content-cell">
                                         {{ Illuminate\Mail\Markdown::parse($slot) }}
 
-                                        {{ $subcopy or '' }}
+                                        {{ $subcopy ?? '' }}
                                     </td>
                                 </tr>
                             </table>
