86bb53288375aa9488de924fb418e723f1e8adef
Mufaddal Kangsawala <mufaddal@systango.com>
2020-03-02 15:59:58 +0530

    debug

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -13,6 +13,9 @@ use InvalidArgumentException;
 
 class Handler extends ExceptionHandler
 {
+    public function __construct()
+        
+    }
     /**
      * A list of the exception types that should not be reported.
      *
@@ -64,7 +67,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        dd(Auth::check());
+        dd(\Auth::check());
         if (env('APP_ENV') == 'local') {
             return parent::render($request, $exception);
         }
