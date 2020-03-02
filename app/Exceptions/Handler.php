f3562588590d5175802721e99c2dda1e0a253172
Mufaddal Kangsawala <mufaddal@systango.com>
2020-03-02 15:44:01 +0530

    debug

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -64,7 +64,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        Log::debug(Auth::check());
+        var_dump(Auth::check());
         if (env('APP_ENV') == 'local') {
             return parent::render($request, $exception);
         }
