5900f66ced93ddfae891d60b5ff322bd5448d038
Mufaddal Kangsawala <mufaddal@systango.com>
2020-03-11 19:53:15 +0530

    reverting class name in left nav

--- resources/views/v1/parts/nav-left.blade.php
+++ resources/views/v1/parts/nav-left.blade.php
@@ -67,7 +67,7 @@ try{
         </div>
         <br>
             <div class="main_menu_side hidden-print main_menu" id="sidebar-menu">
-                <div class="menu_sections">
+                <div class="menu_section">
                     <h3>
                     </h3>
                     <ul class="nav side-menu">
