b45a10dacbd95cb6f41763d33220072ff84f0642
Mufaddal Kangsawala <mufaddal@systango.com>
2020-03-16 20:49:19 +0530

    fixing footer menu

--- resources/views/v1/parts/footer.blade.php
+++ resources/views/v1/parts/footer.blade.php
@@ -58,9 +58,7 @@
     }
     @endif
 </style>
-*/
-@endphp
-<p></p>
+
 <footer>
     <div class="pull-right footer-logo">
         @if(!Auth::check())
@@ -98,4 +96,18 @@
         </ul>
     </div>
     @endif
+</footer>
+
+*/
+@endphp
+
+<p>&nbsp;</p>
+<footer>
+    <div class="pull-right">
+        @if(!Auth::check())
+            <a href="{{ env('APP_URL') }}"><img src="{{ env('APP_URL') }}/images/mCentric_logo_blue.png" alt="mCentric" style="height: 25px;" /></a>
+        @else
+            <img src="{{ env('APP_URL') }}/images/mCentric_logo_blue.png" hspace="50" alt="mCentric" style="height: 25px;" />
+        @endif
+    </div>
 </footer>
