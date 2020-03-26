bed9b0df512c673647583e3dae0152db1b27623e
Mufaddal Kangsawala <mufaddal@systango.com>
2020-03-26 15:02:50 +0530

    yahoo https

--- resources/views/v1/public_pages/event_receipt.blade.php
+++ resources/views/v1/public_pages/event_receipt.blade.php
@@ -53,11 +53,11 @@ $eet = $event->eventEndDate->format('Ymd\THis'); // $event->eventTimeZone;
 
 $dur = sprintf("%02d", $event->eventEndDate->diffInHours($event->eventStartDate)) . "00";
 
-$event_url = trans('messages.email_txt.for_det_visit') . ": " .  urlencode(env('APP_URL') . "/events/$event->slug");
+$event_url = trans('messages.email_txt.for_det_visit') . ": " . env('APP_URL') . "/events/$event->slug";
 $yahoo_url =
-    "http://calendar.yahoo.com/?v=60&TITLE=$event->eventName&DESC=$org->orgName $etype&ST=$est&DUR=$dur&URL=$event_url&in_loc=$loc->locName&in_st=$loc->addr1 $loc->addr2&in_csz=$loc->city, $loc->state $loc->zip";
+    "https://calendar.yahoo.com/?v=60&TITLE=$event->eventName&DESC=$org->orgName $etype&ST=$est&DUR=$dur&URL=$event_url&in_loc=$loc->locName&in_st=$loc->addr1 $loc->addr2&in_csz=$loc->city, $loc->state $loc->zip";
 $google_url =
-    "https://www.google.com/calendar/event?action=TEMPLATE&text=$org->orgName $etype&dates=$est/$eet&name=$event->eventName&details=$event_url&location=$loc->locName $loc->addr1 $loc->addr2 $loc->city, $loc->state $loc->zip";
+    "http://www.google.com/calendar/event?action=TEMPLATE&text=$org->orgName $etype&dates=$est/$eet&name=$event->eventName&details=$event_url&location=$loc->locName $loc->addr1 $loc->addr2 $loc->city, $loc->state $loc->zip";
 $event_filename = 'event_' . $event->eventID . '.ics';
 
 $client = new S3Client([
