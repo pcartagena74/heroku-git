b92af0007def901d5a2a1e0d99008eba9d58f5bd
Mufaddal Kangsawala <mufaddal@systango.com>
2020-03-30 17:06:39 +0530

    adding menu fix bypass

--- resources/views/v1/auth_pages/events/add-edit_form.blade.php
+++ resources/views/v1/auth_pages/events/add-edit_form.blade.php
@@ -558,9 +558,9 @@ try {
         });
     </script>
     @if($page_title == trans('messages.headers.event_edit') || $event === null)
-        @include('v1.parts.menu-fix', array('path' => '/event/create', 'tag' => '#add', 'newTxt' => trans('messages.fields.edit_event')))
+        @include('v1.parts.menu-fix', array('path' => '/event/create', 'tag' => '#add', 'newTxt' => trans('messages.fields.edit_event'),'url_override'=>url('event/create')))
     @else
-        @include('v1.parts.menu-fix', array('path' => '/event/create'))
+        @include('v1.parts.menu-fix', array('path' => '/event/create','url_override'=>url('event/create')))
     @endif
 @endsection
 
