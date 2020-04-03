2f6d8b59dcea69135491f27061187e59e3ba245c
Mufaddal Kangsawala <mufaddal@systango.com>
2020-04-03 17:27:08 +0530

    debug

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2137,16 +2137,15 @@ class UploadController extends Controller
                 // For modularity, updating the $op record will happen below as there are no dependencies
                 // $p = Person::where(['personID' => $op[0]->personID])->get();
                 //
-                $p = Person::where(['personID' => $op->get('personID')])->limit(1)->get()->first();
-                // dd($p);
+                $p = Person::where(['personIDs' => $op->get('personID')])->limit(1)->get();
                 // $this->timeMem('10 op and any op check 2142');
-                // $p = $p[0];
+                $p = $p[0];
             } else {
                 $need_op_record = 1;
                 // $p              = Person::where(['personID' => $any_op[0]->personID])->get();
-                $p = Person::where(['personID' => $any_op->get('personID')])->limit(1)->get()->first();
+                $p = Person::where(['personID' => $any_op->get('personID')])->limit(1)->get();
                 // $this->timeMem('11 op and any op check 2148');
-                // $p = $p[0];
+                $p = $p[0];
             }
 
             // We have an $org-person record so we should NOT rely on firstName/lastName matching at all
