9cdc7a23a38ea34c4bd4fc7243a7f2d3a8b72a53
Mufaddal Kangsawala <mufaddal@systango.com>
2020-04-08 12:44:06 +0530

    changing path

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -156,8 +156,7 @@ class UploadController extends Controller
                     $currentPerson = Person::where('personID', auth()->user()->id)->get()->first();
                     // Excel::queueImport(new MembersImport($currentPerson), $path)->chain([Notification::route('mail', $currentPerson->login)->notify(new MemeberImportExcelNotification())]);
                     $import = new MembersImport($currentPerson);
-                    $import->path($path);
-                    Excel::queueImport($import, $path)
+                    Excel::queueImport($import, $file)
                         ->chain([new NotifyUserOfCompletedImport($currentPerson,$import->getProcessedRowCount())]);
                     request()->session()->flash('alert-success', trans('messages.messages.import_file_queued'));
 
