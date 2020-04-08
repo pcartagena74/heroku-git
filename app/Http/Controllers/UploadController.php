f1a493757c3522ac55294aac2deec3e756295ffb
Mufaddal Kangsawala <mufaddal@systango.com>
2020-04-08 16:27:36 +0530

    debug queue

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -157,7 +157,7 @@ class UploadController extends Controller
                     $currentPerson = Person::where('personID', auth()->user()->id)->get()->first();
                     // Excel::queueImport(new MembersImport($currentPerson), $path)->chain([Notification::route('mail', $currentPerson->login)->notify(new MemeberImportExcelNotification())]);
                     $import = new MembersImport($currentPerson);
-                    Excel::queueImport($import, $file_name,'local')
+                    Excel::queueImport(new MembersImport($currentPerson), $file_name,'local')
                         ->chain([new NotifyUserOfCompletedImport($currentPerson,$import->getProcessedRowCount())]);
                     request()->session()->flash('alert-success', trans('messages.messages.import_file_queued'));
 
