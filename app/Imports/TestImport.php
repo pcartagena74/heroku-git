3246ebb2bb06e9d743492bd993b9c2892c676745
Mufaddal Kangsawala <mufaddal@systango.com>
2020-04-10 17:59:00 +0530

    queue tinkering

--- app/Imports/TestImport.php
+++ app/Imports/TestImport.php
@@ -27,8 +27,8 @@ class TestImport implements ToCollection, WithChunkReading, WithHeadingRow, With
      */
     public function collection(Collection $collection)
     {
+            dd('should execute this');
         foreach ($collection as $row) {
-            requestBin($row->toArray());
 
         }
 
