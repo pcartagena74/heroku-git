cbcdd48725305917c7ca83c2e289d64b040325a3
Mufaddal Kangsawala <mufaddal@systango.com>
2020-05-12 18:02:51 +0530

    WIP: updated industry breakdown chart with industry toggle, added map commented till get account details, wip bar chart

--- resources/views/v1/auth_pages/members/mbr_report.blade.php
+++ resources/views/v1/auth_pages/members/mbr_report.blade.php
@@ -1,4 +1,4 @@
-<?php
+@php
 /**
  * Comment: Report showing member activity by year and industry breakdown
  * Created: 2/9/2017
@@ -6,60 +6,103 @@
  * Updated: 10/4/2019 for configurable graph (display years)
  *
  */
-?>
+@endphp
 @extends('v1.layouts.auth', ['topBits' => $topBits])
 
 @section('content')
-    <ul id="myTab" class="nav nav-tabs bar_tabs nav-justified" role="tablist">
-        <li class="active"><a href="#tab_content1" id="member_tab" data-toggle="tab"
-                              aria-expanded="true"><b>@lang('messages.tabs.mem_demo')</b></a></li>
-        <li class=""><a href="#tab_content2" id="everyone_tab" data-toggle="tab"
-                        aria-expanded="false"><b>@lang('messages.tabs.heat_map')</b></a></li>
+<link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet"/>
+<div class="col-md-12 col-sm-12 col-xs-12">
+    <ul class="nav nav-tabs bar_tabs nav-justified" id="myTab" role="tablist">
+        <li class="active">
+            <a aria-expanded="true" data-toggle="tab" href="#tab_content1" id="member_tab">
+                <b>
+                    @lang('messages.tabs.mem_demo')
+                </b>
+            </a>
+        </li>
+        <li class="">
+            <a aria-expanded="false" data-toggle="tab" href="#tab_content2" id="everyone_tab">
+                <b>
+                    @lang('messages.tabs.heat_map')
+                </b>
+            </a>
+        </li>
         {{--
                     @if(Entrust::hasRole('Speaker'))
-                        <li class=""><a href="#tab_content3" id="other-tab" data-toggle="tab"
-                                        aria-expanded="false"><b>Third Thing</b></a></li>
-                    @endif
+        <li class="">
+            <a aria-expanded="false" data-toggle="tab" href="#tab_content3" id="other-tab">
+                <b>
+                    Third Thing
+                </b>
+            </a>
+        </li>
+        @endif
         --}}
     </ul>
-    <p>&nbsp;</p>
-
-    <div id="tab-content" class="tab-content">
-        <div class="tab-pane active" id="tab_content1" aria-labelledby="member_tab">
-            &nbsp;<br/>
+    <div class="tab-content" id="tab-content">
+        <div aria-labelledby="member_tab" class="tab-pane active" id="tab_content1">
+            <br/>
             @include('v1.parts.start_content', ['header' => trans('messages.reports.ev_by_year'), 'subheader' => '',
                      'w1' => '6', 'w2' => '12', 'r1' => 0, 'r2' => 0, 'r3' => 0])
+            <b>
+                @lang('messages.reports.graph_years'):
+            </b>
+            <a data-pk="{{ 1 }}" data-title="{{ trans('messages.reports.select_years') }}" data-type="select2" id="tags">
+                {!! $year_string !!}
+            </a>
+            <select class="select2" multiple="multiple">
+                @php 
+                foreach(explode(',',$year_string) as $key=>$value){
+                    echo '
+                <option value="'.$value.'">
+                    '.$value.'
+                </option>
+                ';
 
-                <b>@lang('messages.reports.graph_years'):</b> <a id="tags"
-                                       data-pk="{{ 1 }}" data-type="select2" data-title="{{ trans('messages.reports.select_years') }}"
-                                       data-url="{{ env('APP_URL') }}/mbrreport/{{ 1 }}">{!! $year_string !!}</a><br/>
-
-            <div id="canvas"></div>
-
+                }
+                @endphp
+            </select>
+            <br/>
+            <div id="canvas">
+            </div>
             @include('v1.parts.end_content')
 
             @include('v1.parts.start_content', ['header' => trans('messages.reports.ind_brk'), 'subheader' => '',
                      'w1' => '6', 'w2' => '12', 'r1' => 0, 'r2' => 0, 'r3' => 0])
             <div class="col-md-12 col-sm-12 col-xs-12">
-                <canvas id="indPie"></canvas>
+                <canvas id="indPie">
+                </canvas>
             </div>
-            <div id="pieLegend" class="col-md-12 col-sm-12 col-xs-12">
+            <div class="col-md-12 col-sm-12 col-xs-12" id="pieLegend">
             </div>
             @include('v1.parts.end_content')
-
-
         </div>
-        <div class="tab-pane fade" id="tab_content2" aria-labelledby="everyone_tab">
-            <br/>
+        <div aria-labelledby="everyone_tab" class="tab-pane fade" id="tab_content2">
+            <div id="floating-panel">
+                <button onclick="toggleHeatmap()">
+                    Toggle Heatmap
+                </button>
+                <button onclick="changeGradient()">
+                    Change gradient
+                </button>
+                <button onclick="changeRadius()">
+                    Change radius
+                </button>
+                <button onclick="changeOpacity()">
+                    Change opacity
+                </button>
+            </div>
+            <div id="map">
+            </div>
         </div>
     </div>
-
+</div>
 @endsection
 
 @section('scripts')
     @include('v1.parts.menu-fix', array('path' => '/mbrreport'))
-    <script>
-        // $.fn.editable.defaults.ajaxOptions = {type: "POST"};
+<script>
+    // $.fn.editable.defaults.ajaxOptions = {type: "POST"};
         var result;
         $.ajaxSetup({
             headers: {
@@ -75,32 +118,152 @@
                 tokenSeparators: [",", " "]
             },
             success: function (data) {
+                // updateMorrisChart(data);
                 result = eval(data);
-                window.location = "{{ env('APP_URL') . "/mbrreport/$orgID" }}";
+                // window.location = "{{ env('APP_URL') . "/mbrreport/$orgID" }}";
             },
         });
-    </script>
-    @include('v1.parts.footer-chart')
-    <script>
-        $(document).ready(function () {
-            Morris.Bar({
-                element: 'canvas',
-                data: [
-                    {!! $datastring !!}
-                ],
-                xkey: '{{ trans_choice('messages.headers.events', 2) }}',
-                ykeys: [ {!! $labels !!} ],
-                labels: [ {!! $labels !!} ],
-                barRatio: 0.1,
-                xLabelAngle: 35,
-                hideHover: 'auto',
-                resize: true
+        $('.select2').select2();
+</script>
+@include('v1.parts.footer-chart')
+<script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js">
+</script>
+<script>
+    var morris = '';
+    $(document).ready(function () {
+        morris = Morris.Bar({
+            element: 'canvas',
+            data: [{!! $datastring !!}],
+            xkey: '{{ trans_choice('messages.headers.events', 2) }}',
+            ykeys: [ {!! $labels !!} ],
+            labels: [ {!! $labels !!} ],
+            barRatio: 0.1,
+            xLabelAngle: 35,
+            hideHover: 'auto',
+            resize: true,
+        });
+    });
+    $('#tags').on('shown', function(e, editable) {
+
+        var popover = editable.input.$input[0].closest('.popover');
+        var popover_id = popover.id;
+        
+        $(document).on('change', editable, function() {
+            var new_value = editable.input.$input[0].value;
+            // console.log(new_value);
+            updateMorrisChart(new_value);
+        });
+    });
+
+    function morrisData(data) {
+        if(data){
+            return data;
+        } else {
+            return [{!! $datastring !!}];
+        }
+    }
+    function updateMorrisChart(data){
+        var morris_data_real = [{!! $datastring !!}];
+        var list = data.split(',');
+        $.each(morris_data_real,function(key,value){
+            $.each(value,function(key_in,value_in){
+                if(list.indexOf(key_in) === -1 && key_in != 'Events'){
+                    delete morris_data_real[key][key_in];
+                }
             });
         });
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js"></script>
-    <script>
-        var ctx = document.getElementById("indPie").getContext('2d');
+        morris.options.ykeys = list;
+        morris.options.labels = list;
+        morris.setData(morris_data_real);
+    }
+    var map, hesatmap;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: {lat: 37.775, lng: -122.434},
+          mapTypeId: 'satellite'
+        });
+
+        heatmap = new google.maps.visualization.HeatmapLayer({
+          data: getPoints(),
+          map: map
+        });
+      }
+
+      function toggleHeatmap() {
+        heatmap.setMap(heatmap.getMap() ? null : map);
+      }
+
+      function changeGradient() {
+        var gradient = [
+          'rgba(0, 255, 255, 0)',
+          'rgba(0, 255, 255, 1)',
+          'rgba(0, 191, 255, 1)',
+          'rgba(0, 127, 255, 1)',
+          'rgba(0, 63, 255, 1)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 223, 1)',
+          'rgba(0, 0, 191, 1)',
+          'rgba(0, 0, 159, 1)',
+          'rgba(0, 0, 127, 1)',
+          'rgba(63, 0, 91, 1)',
+          'rgba(127, 0, 63, 1)',
+          'rgba(191, 0, 31, 1)',
+          'rgba(255, 0, 0, 1)'
+        ]
+        heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+      }
+
+      function changeRadius() {
+        heatmap.set('radius', heatmap.get('radius') ? null : 20);
+      }
+
+      function changeOpacity() {
+        heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+      }
+
+      // Heatmap data: 500 Points
+      function getPoints() {
+        return [
+          new google.maps.LatLng(37.776772, -122.438498),
+          new google.maps.LatLng(37.776787, -122.438389),
+          new google.maps.LatLng(37.776848, -122.438283),
+          new google.maps.LatLng(37.776870, -122.438239),
+          new google.maps.LatLng(37.777015, -122.438198),
+          new google.maps.LatLng(37.777333, -122.438256),
+          new google.maps.LatLng(37.777595, -122.438308),
+          new google.maps.LatLng(37.777797, -122.438344),
+          new google.maps.LatLng(37.778160, -122.438442),
+          new google.maps.LatLng(37.778414, -122.438508),
+          new google.maps.LatLng(37.778445, -122.438516),
+          new google.maps.LatLng(37.778503, -122.438529),
+          new google.maps.LatLng(37.778607, -122.438549),
+          new google.maps.LatLng(37.778670, -122.438644),
+          new google.maps.LatLng(37.778847, -122.438706),
+          new google.maps.LatLng(37.779240, -122.438744),
+          new google.maps.LatLng(37.779738, -122.438822),
+          new google.maps.LatLng(37.758182, -122.405695),
+          new google.maps.LatLng(37.757676, -122.405118),
+          new google.maps.LatLng(37.757039, -122.404346),
+          new google.maps.LatLng(37.756335, -122.403719),
+          new google.maps.LatLng(37.755503, -122.403406),
+          new google.maps.LatLng(37.754665, -122.403242),
+          new google.maps.LatLng(37.753837, -122.403172),
+          new google.maps.LatLng(37.752986, -122.403112),
+          new google.maps.LatLng(37.751266, -122.403355)
+        ];
+      }
+</script>
+{{--
+<script async="" defer="" src="https://maps.googleapis.com/maps/api/js?key={{env('GOOGLE_API_KEY')}}&callback=initMap" type="text/javascript">
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key={{env('GOOGLE_API_KEY')}}&libraries=visualization">
+</script>
+--}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js">
+</script>
+<script>
+    var ctx = document.getElementById("indPie").getContext('2d');
         var options = {
             responsive: true,
             legend: {
@@ -191,7 +354,7 @@
                     var text = [];
                     text.push('<ul>');
                     for (var i = 0; i < chart.data.datasets[0].data.length; i++) {
-                        text.push('<li>');
+                        text.push('<li data-dataset="'+i+'">');
                         text.push('<span style="color:white; background-color:'
                             + chart.data.datasets[0].backgroundColor[i] + '">&nbsp;'
                             + chart.data.datasets[0].data[i] + ' </span> &nbsp;');
@@ -206,5 +369,15 @@
             }
         });
         document.getElementById('pieLegend').innerHTML = myChart.generateLegend();
-    </script>
+        $('#pieLegend ul li').on('click',function(){
+            if(myChart.getDatasetMeta(0).data[$(this).data('dataset')].hidden == true){
+                myChart.getDatasetMeta(0).data[$(this).data('dataset')].hidden = false;
+                $(this).unwrap();
+            } else {
+                myChart.getDatasetMeta(0).data[$(this).data('dataset')].hidden = true;
+                $(this).wrap("<strike>");
+            }
+            myChart.update();
+        });
+</script>
 @endsection
