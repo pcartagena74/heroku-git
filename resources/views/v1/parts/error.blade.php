4ee6ab9ffad10eef96f5232c884b5c6023475e47
Mufaddal Kangsawala <mufaddal@systango.com>
2020-06-22 11:09:26 +0530

    UI fixes: error notification and become notice overlapping fixed

--- resources/views/v1/parts/error.blade.php
+++ resources/views/v1/parts/error.blade.php
@@ -13,27 +13,30 @@
     .error {color:red;}
 </style>
 --}}
-<div class="form-group">
+
     @foreach (['danger', 'warning', 'success', 'info'] as $msg)
         @if(Session::has('alert-'.$msg))
+<div class="form-group" style="margin-top:55px;">
     <div class="alert alert-{{ $msg }}">
         <a aria-label="close" class="close" data-dismiss="alert" href="#">
             ×
         </a>
         {!!  Session::get('alert-' . $msg) !!}
     </div>
-    @endif
+</div>
+@endif
     @endforeach
     @if(isset($errors))
     @if(count($errors))
-        @foreach ($errors->all() as $e)
+<div class="form-group" style="margin-top:55px;">
+    @foreach ($errors->all() as $e)
     <div class="alert alert-danger">
         <a aria-label="close" class="close" data-dismiss="alert" href="#">
             ×
         </a>
         {!! $e !!}
     </div>
-    @endforeach
-    @endif
-    @endif
 </div>
+@endforeach
+    @endif
+    @endif
