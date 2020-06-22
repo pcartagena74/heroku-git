73c25f917edb7a47c1cd58135dd8207e92eb30b5
Mufaddal Kangsawala <mufaddal@systango.com>
2020-06-22 11:25:50 +0530

    UI Fix: removed extra margin if become and error come together

--- resources/views/v1/parts/error.blade.php
+++ resources/views/v1/parts/error.blade.php
@@ -1,4 +1,4 @@
-<?php
+@php
 /**
  * Comment: A default error display bit
  * Created: 2/20/2017
@@ -7,7 +7,12 @@
  * Session::flash('alert-$X', 'The message to display...');
  *
  */
-?>
+// Session::has('become')
+$margin = 'margin-top:55px;';
+if(Session::has('become')){
+    $margin = '';
+}
+@endphp
 {{--
 <style>
     .error {color:red;}
@@ -16,7 +21,7 @@
 
     @foreach (['danger', 'warning', 'success', 'info'] as $msg)
         @if(Session::has('alert-'.$msg))
-<div class="form-group" style="margin-top:55px;">
+<div class="form-group" style="{{$margin}}">
     <div class="alert alert-{{ $msg }}">
         <a aria-label="close" class="close" data-dismiss="alert" href="#">
             ×
@@ -28,7 +33,12 @@
     @endforeach
     @if(isset($errors))
     @if(count($errors))
-<div class="form-group" style="margin-top:55px;">
+    @php
+    if(!empty($msg)){
+        $margin = '';
+    }
+    @endphp
+<div class="form-group" style="{{$margin}}">
     @foreach ($errors->all() as $e)
     <div class="alert alert-danger">
         <a aria-label="close" class="close" data-dismiss="alert" href="#">
