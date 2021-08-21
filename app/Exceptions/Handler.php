ebd63b2c80f45134b83527298b7d870fea8204a2
Laravel Shift <shift@laravelshift.com>
2021-08-21 17:24:50 +0000

    Shift exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -37,6 +37,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontFlash = [
+        'current_password',
         'password',
         'password_confirmation',
     ];
