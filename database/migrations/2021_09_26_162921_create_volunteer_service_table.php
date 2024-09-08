48b478a5addc416936c3e466e941e47652344e99
Shift <shift@laravelshift.com>
2024-09-08 17:08:56 +0000

    Adopt anonymous migrations

--- database/migrations/2021_09_26_162921_create_volunteer_service_table.php
+++ database/migrations/2021_09_26_162921_create_volunteer_service_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateVolunteerServiceTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -40,4 +40,4 @@ class CreateVolunteerServiceTable extends Migration
     {
         Schema::dropIfExists('volunteer_service');
     }
-}
+};
