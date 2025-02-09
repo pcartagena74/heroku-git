42811e38be496866c1ff848d65c317c41fd63194
Shift <shift@laravelshift.com>
2025-02-09 23:40:35 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Jobs\SendCampaignEmail;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})->purpose('Display an inspiring quote');
+
+
+// Heroku supports only 10 mins we will take care of reset from job itself
+Schedule::job(new SendCampaignEmail)->everyThirtyMinutes();
