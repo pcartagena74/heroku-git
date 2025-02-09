ee308455d91c1c0ffeebbb45bd34eb4586d7244b
Shift <shift@laravelshift.com>
2025-02-09 23:40:33 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,34 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->encryptCookies(except: [
+            //
+        ]);
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+            'reportissue',
+        ]);
+
+        $middleware->append(\App\Http\Middleware\Locale::class);
+
+        $middleware->web(\App\Http\Middleware\Locale::class);
+
+        $middleware->throttleApi();
+
+        $middleware->replace(\Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class, \App\Http\Middleware\PreventRequestsDuringMaintenance::class);
+        $middleware->replace(\Illuminate\Foundation\Http\Middleware\TrimStrings::class, \App\Http\Middleware\TrimStrings::class);
+        $middleware->replace(\Illuminate\Http\Middleware\TrustHosts::class, \App\Http\Middleware\TrustHosts::class);
+        $middleware->replace(\Illuminate\Http\Middleware\TrustProxies::class, \App\Http\Middleware\TrustProxies::class);
+
+        $middleware->alias([
+            'ability' => \Shanmuga\LaravelEntrust\Middleware\LaravelEntrustAbility::class,
+            'permission' => \Shanmuga\LaravelEntrust\Middleware\LaravelEntrustPermission::class,
+            'role' => \Shanmuga\LaravelEntrust\Middleware\LaravelEntrustRole::class,
+            'signed' => \App\Http\Middleware\ValidateSignature::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
